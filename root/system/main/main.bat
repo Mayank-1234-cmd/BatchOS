b90b43e537764b8a60a2b15a38729b46a9380a3a
Scy Productions <wiktorpl717@gmail.com>
2018-12-19 11:21:27 +0100

    Start from source + small bug fixes

--- root/system/main/main.bat
+++ root/system/main/main.bat
@@ -6,10 +6,12 @@ goto start
 
 :start
 title %name% ver. %ver%
-if not "%~dp0"=="%TEMP%" call warning.bat rfs
+if not "%~dp0"=="%TEMP%" call %~dp0warning.bat rfs
 echo Hello! You've successfully started up BatchOS!
 echo If you're running from the source, you can make changes to the code.
 echo If you're not though, you'll need to download the source code.
 echo Don't worry! The code isn't that big in size, so you can download the whole BatchOS and compile it for free!
 
 :end
+echo End of file -- pausing
+pause>nul
---
+++ startfromsource.bat
@@ -0,0 +1,3 @@
+@echo off
+title startfromsource
+call %~dp0root\system\main\main.bat
